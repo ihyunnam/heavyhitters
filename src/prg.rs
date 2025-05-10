eb38b65f88045798a0a62e2d5844425a309f3420
Ihyun Nam <ihyun@stanford.edu>
2025-05-10 23:07:18 +0900

    prg.rs import errors

--- src/prg.rs
+++ src/prg.rs
@@ -1,4 +1,4 @@
-// use aes::cipher::{NewStreamCipher, SyncStreamCipher};
+use aes::cipher::{NewStreamCipher, SyncStreamCipher};
 
 use core::arch::x86_64::{
     __m128i, _mm_add_epi64, _mm_loadu_si128, _mm_set_epi64x, _mm_storeu_si128,
@@ -6,7 +6,8 @@ use core::arch::x86_64::{
 
 // use aes::block_cipher::{generic_array::GenericArray, Block, BlockCipher, NewBlockCipher};
 use aes::{Aes128, BlockCipher, NewBlockCipher};
-use aes_ctr::stream_cipher::{NewStreamCipher, SyncStreamCipher};
+// use aes_ctr::stream_cipher::{NewStreamCipher, SyncStreamCipher};
+// use aes::cipher::NewStreamCipher;
 use aes_ctr::Aes128Ctr;
 use aes::cipher::generic_array::{GenericArray, Block};
 
